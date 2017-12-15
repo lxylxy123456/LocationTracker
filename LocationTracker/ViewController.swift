3449ef920a255c2629e54e1230fe4c2758790273
李宵逸 <lxy@campus-098-235.ucdavis.edu>
2017-12-15 12:00:14 -0800

    Initial Commit

---
+++ LocationTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationTracker
+//
+//  Created by 李宵逸 on 12/15/17.
+//  Copyright © 2017 lxylxy123456. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LocationTrackerTests/LocationTrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  LocationTrackerTests.swift
+//  LocationTrackerTests
+//
+//  Created by 李宵逸 on 12/15/17.
+//  Copyright © 2017 lxylxy123456. All rights reserved.
+//
+
+import XCTest
+@testable import LocationTracker
+
+class LocationTrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
